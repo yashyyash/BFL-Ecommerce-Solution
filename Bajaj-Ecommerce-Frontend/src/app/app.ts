7bb132fb78246f37ca4113d8f8d9043e97b25ac5
Yash <yashnil532@gmail.com>
2025-10-25 01:33:23 +0530

    Completed Shared Components: Navbar, Slider, Banner , Footer

--- Bajaj-Ecommerce-Frontend/src/app/app.ts
+++ Bajaj-Ecommerce-Frontend/src/app/app.ts
@@ -1,11 +1,17 @@
 import { Component, signal } from '@angular/core';
+import { SideNavbar } from './shared/components/side-navbar/side-navbar';
+import { Slider } from './shared/components/slider/slider';
+import { Banner } from './shared/components/banner/banner';
+import { Footer } from './shared/components/footer/footer';
 
 @Component({
   selector: 'bajaj-root',
-  imports: [],
+   imports: [SideNavbar, Slider, Banner, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('Bajaj-ecommerece-app');
 }
+
+
